8c92a6df2acd70872688d866a792abb6ff53ba8d
saffr3n <saffr3n@gmail.com>
2025-07-12 02:01:45 +0300

    feat: create 16x16 grid

---
+++ script.js
@@ -0,0 +1,9 @@
+(() => {
+  const GRID_SIZE = 16;
+  const container = document.querySelector(".container");
+  for (let i = 0, n = GRID_SIZE * GRID_SIZE; i < n; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+})();
