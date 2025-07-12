14a327d8b7ebbb46879ad1d89db7ed12954ff985
saffr3n <saffr3n@gmail.com>
2025-07-12 15:52:57 +0300

    feat: add grid resize functionality

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 (() => {
-  const GRID_SIZE = 16;
+  let gridSize = 16;
+  const sizeBtn = document.querySelector('button');
   const container = document.querySelector('.container');
-  for (let i = 0, n = GRID_SIZE * GRID_SIZE; i < n; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
+
+  function drawGrid() {
+    container.innerHTML = '';
+    for (let i = 0, n = gridSize * gridSize; i < n; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.style.width = `calc(100% / ${gridSize})`;
+      cell.style.height = `calc(100% / ${gridSize})`;
+      container.appendChild(cell);
+    }
   }
+  drawGrid();
+
+  sizeBtn.onclick = () => {
+    let input;
+    while (true) {
+      input = prompt('New grid size (max 100): ');
+      const num = Number(input);
+      if (!isNaN(num) && num > 0 && num <= 100) {
+        gridSize = Math.floor(num);
+        break;
+      }
+    }
+    drawGrid();
+  };
 
   container.addEventListener('mouseover', (e) => {
     if (e.target.className !== 'cell') return;
