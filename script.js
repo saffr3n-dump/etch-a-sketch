67bbfba3adc975b3f11e3c3508ccaddbbedc434c
saffr3n <saffr3n@gmail.com>
2025-07-12 14:45:18 +0300

    feat: change grid cell color on hover

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 (() => {
   const GRID_SIZE = 16;
-  const container = document.querySelector(".container");
+  const container = document.querySelector('.container');
   for (let i = 0, n = GRID_SIZE * GRID_SIZE; i < n; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
     container.appendChild(cell);
   }
+
+  container.addEventListener('mouseover', (e) => {
+    if (e.target.className !== 'cell') return;
+    e.target.style.backgroundColor = 'black';
+  });
 })();
